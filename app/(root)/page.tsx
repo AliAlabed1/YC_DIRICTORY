56d3904667b119f14263146ca93c86454123c446
ALIALABED1 <alialabed161@gmail.com>
2024-11-14 15:25:44 +0300

    initial-commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup <br /> Connect with Ent repreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and git noticed in virtual Competitions.
+        </p>
+        
+
+      </section>
+      
+    </>
+  );
+}

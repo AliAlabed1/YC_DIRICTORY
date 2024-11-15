e376034dca33bcb67d1cc36b4a732281fd9ed7db
ALIALABED1 <alialabed161@gmail.com>
2024-11-15 21:32:44 +0300

    add startup card and some things to frontend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
+  const query = (await searchParams).query
+  const posts = [
+    {
+      _createdAt:new Date(),
+      views:55,
+      author:{
+        _id:1,
+        name:'Ali'
+      },
+      _id:1,
+      description:'This is the desc',
+      image:'logo.png',
+      category:'ropots',
+      title:'we robots'
+    }
+  ]
   return (
     <>
       <section className="pink_container">
@@ -10,8 +28,20 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and git noticed in virtual Competitions.
         </p>
-        
+        <SearchForm query={query}/>
+      </section>
 
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query?`Search results for "${query}"`:"All Startups"}
+        </p>
+        <ul className="mt-7  card_grid">
+          {
+            posts?.length>0 && posts.map((post:StartupCardType,index:number)=>(
+              <StartupCard key={post._id} post ={post}/>
+            ))
+          }
+        </ul>
       </section>
       
     </>
